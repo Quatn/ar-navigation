cd27342926d4f248b4bf25d11aaffc0c43d10be4
dangvh <dangvh@bravebits.vn>
2024-04-14 11:07:48 +0700

    refine y-axis

--- src/data.js
+++ src/data.js
@@ -2,21 +2,21 @@ export default {
   AlphaR102: {
     position: {
       x: -20,
-      y: 0,
+      y: -3,
       z: 4,
     },
   },
   Showroom: {
     position: {
       x: -40,
-      y: 0,
+      y: -3,
       z: 4,
     },
   },
   marker: {
     position: {
       x: -40,
-      y: 0,
+      y: -3,
       z: 4,
     },
   },
